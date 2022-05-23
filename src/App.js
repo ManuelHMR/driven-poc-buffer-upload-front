fe367d9bedac2951559b89e3e731fc1165070174
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-23 19:22:49 -0300

    poc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState();
+  const [file, setFile] = useState();
+
+  const send = event => {
+    const data = new FormData(); // Para enviar 
+    data.append("name", name);
+    data.append("fotos", file);
+
+    axios.post("http://localhost:5000/upload", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="#">
+          <div className="flex">
+            <label htmlFor="name">Nome</label>
+            <input
+              type="text"
+              id="name"
+              onChange={event => {
+                const { value } = event.target;
+                setName(value);
+              }}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="file">Arquivo</label>
+            <input
+              type="file"
+              id="file"
+              onChange={event => {
+                const file = event.target.files[0]; // IMPORTANTE
+                setFile(file);
+              }}
+            />
+          </div>
+        </form>
+        <button onClick={send}>Send</button>
       </header>
     </div>
   );
